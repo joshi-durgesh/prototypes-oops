a420cfd83d7af2396eced9eeed7524a2e17efbfc
joshi-durgesh <durgesh7joshi9@gmail.com>
2023-12-27 20:01:04 +0530

    methods in classes and constructor

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-// class AgedPerson {
-//   printAge() {
-//     console.log(this.age);
-//   }
-// }
+class Person {
+  name = "Durgesh";
 
-// class Person extends AgedPerson {
-//   name = "Durgesh";
+  constructor() {
+    this.age = 23;
+  }
 
-//   constructor() {
-//     super();
-//     this.age = 23;
-//   }
-
-//   greet() {
-//     console.log(`Hi, I am ${this.name} and I am ${this.age} years old.`);
-//   }
-// }
-
-function Person() {
-  this.age = 23;
-  this.name = "Durgesh";
-  this.greet = function () {
+  greet() {
     console.log(`Hi, I am ${this.name} and I am ${this.age} years old.`);
-  };
+  }
 }
+
+// function Person() {
+//   this.age = 23;
+//   this.name = "Durgesh";
+//   this.greet = function () {
+//     console.log(`Hi, I am ${this.name} and I am ${this.age} years old.`);
+//   };
+// }
 
 // Person.prototype = {
 //   printAge() {
@@ -31,16 +24,24 @@ function Person() {
 //   },
 // };
 
-Person.prototype.printAge = function () {
-  console.log(this.age);
-};
+// Person.prototype.printAge = function () {
+//   console.log(this.age);
+// };
 
-console.dir(Person);
+// console.dir(Person);
+
+// const p = new Person();
+// p.greet();
+// p.printAge();
+// console.log(p.__proto__);
+// console.log(p.toString());
+// const p2 = new p.__proto__.constructor();
+// console.dir(Object.prototype.__proto__);
 
 const p = new Person();
+const p2 = new Person();
 p.greet();
-p.printAge();
-console.log(p.__proto__);
-console.log(p.toString());
-const p2 = new p.__proto__.constructor();
-console.dir(Object.prototype.__proto__);
+console.log(p);
+
+const button = document.getElementById("btn");
+button.addEventListener("click", p.greet.bind(p));
