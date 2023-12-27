4a7f7d03f395e18f200d47121baeeb72501b7551
joshi-durgesh <durgesh7joshi9@gmail.com>
2023-12-27 21:03:11 +0530

    gettin and setting prototype

--- app.js
+++ app.js
@@ -38,10 +38,54 @@ class Person {
 // const p2 = new p.__proto__.constructor();
 // console.dir(Object.prototype.__proto__);
 
-const p = new Person();
-const p2 = new Person();
-p.greet();
-console.log(p);
+// const p = new Person();
+// const p2 = new Person();
+// p.greet();
+// console.log(p);
 
-const button = document.getElementById("btn");
-button.addEventListener("click", p.greet.bind(p));
+// const button = document.getElementById("btn");
+// button.addEventListener("click", p.greet.bind(p));
+
+const course = {
+  title: "Javascript the complete guide",
+  rating: 5,
+};
+
+console.log(Object.getPrototypeOf(course));
+Object.setPrototypeOf(course, {
+  // ...Object.getPrototypeOf(course),
+  printRating: function () {
+    console.log(`${this.rating}/5`);
+  },
+});
+
+const student = Object.create(
+  {
+    printProgress: function () {
+      console.log(this.progress);
+    },
+  },
+  {
+    name: {
+      configurable: true,
+      enumerable: true,
+      value: "Max",
+      writable: true,
+    },
+  }
+);
+
+// student.name = "durgesh";
+
+// Object.defineProperty(student, "progress", {
+//   configurable: true,
+//   enumerable: true,
+//   value: 0.8,
+//   writable: false,
+// });
+
+console.log(student);
+
+// console.log(course.__proto__);
+
+course.printRating();
